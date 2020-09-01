2d5c4de661a04be7138f8ab4c9fdeb03f6270ead
Tony Lau <ual_ynot@yahoo.com>
2020-09-01 01:33:20 -0700

    Updated allocate function and added std.h for some utility functions defined in assembly

--- src/c/alloc.c
+++ src/c/alloc.c
@@ -24,8 +24,9 @@ struct chunk_entry {
   // (flags & 0x1): 1 denotes that entry is valid and in use, 0 denotes entry is free
   struct chunk_entry* prev;     // pointer to prev chunk entry/prev free entry
   struct chunk_entry* next;     // pointer to next chunk entry/next free entry
-  uintptr_t min;
-  uintptr_t max;
+  uintptr_t min;                // points to first byte in chunk
+  uintptr_t max;                // points to first byte not in chunk
+  uint32_t  align;              // minimum align of memory allocated from chunk
   uint32_t  fsize;              // bytes remaining in chunk
   uint8_t   fmap[BYTEMAP_SIZE]; // byte map of free space, 0 = free
 };
@@ -97,14 +98,14 @@ struct alloc_entry* insert_alloc(struct alloc_entry* parent, uint32_t align) {
 }
 
 /* inserts a new chunk entry to the specified allocator entry.
- * returns 1 if successful, 0 if failure
+ * returns the new chunk_entry if successful, NULL if failure
  */
-uint32_t insert_chunk(struct alloc_entry* alloc, uintptr_t min, uintptr_t max) {
+struct chunk_entry* insert_chunk(struct alloc_entry* alloc, uintptr_t min, uintptr_t max) {
   // pop head from free list
   struct chunk_entry* chunk = alloc->fhead;
   if (chunk == NULL) {
     // fail if free list empty
-    return 0;
+    return NULL;
   }
   alloc->fhead = chunk->next;
 
@@ -114,6 +115,7 @@ uint32_t insert_chunk(struct alloc_entry* alloc, uintptr_t min, uintptr_t max) {
   chunk->next  = NULL;
   chunk->min   = min;
   chunk->max   = max;
+  chunk->align = alloc->align;
   chunk->fsize = max - min;
   memset(chunk->fmap, 0, BYTEMAP_SIZE);
 
@@ -126,7 +128,7 @@ uint32_t insert_chunk(struct alloc_entry* alloc, uintptr_t min, uintptr_t max) {
   alloc->utail = chunk;
   alloc->nchunks += 1;
   alloc->fsize += chunk->fsize;
-  return 1;
+  return chunk;
 }
 
 // initialize global allocator tree
@@ -148,6 +150,43 @@ void init_alloc_tree() {
   insert_chunk(alloc, MEM_MIN, MEM_MAX);
 }
 
+/* assumes align is a power of 2 and greater than or equal to allocator align
+ * returns address if allocation successful, returns NULL if fails
+ */
+void* allocate_internal_chunk(struct chunk_entry* chunk, uint32_t size, uint32_t align) {
+  if (size > chunk->fsize) {
+    // if not enough space, fail immediately
+    return NULL;
+  }
+
+  // look for contiguous memory of requisite size and alignment
+  uint32_t contig = 0; // counts contiguous memory areas with size allocator->align
+  for (uint32_t i = 0; i < BYTEMAP_SIZE; i++) {
+    if (chunk->fmap[i] == 0 && (contig > 0 || i % (align / chunk->align) == 0)) {
+      /* increment contiguous counter if:
+       *   -mem location is free and already counting (is contiguous with already counted mem)
+       *   -mem location is free and alignment matches (can start counting new contiguous mem)
+       */
+      contig++;
+    } else {
+      // otherwise reset contiguous counter
+      contig = 0;
+    }
+    // check if enough contiguous memory found
+    if (contig * chunk->align >= size) {
+      // mark as used
+      for (uint32_t j = i - contig + 1; j <= i; j++) {
+        chunk->fmap[j] = 1;
+      }
+      // decrease free mem counters
+      chunk->fsize -= contig * chunk->align;
+      // alloc->fsize -= contig * chunk->align; TODO propagate upwards
+      return (void*) (chunk->min + chunk->align * (i - contig + 1));
+    }
+  }
+  return NULL;
+}
+
 // main allocate method
 void* allocate_internal(struct alloc_entry* alloc, uint32_t size, uint32_t align) {
   if (alloc == NULL) {
@@ -155,68 +194,65 @@ void* allocate_internal(struct alloc_entry* alloc, uint32_t size, uint32_t align
     return NULL;
   }
 
-  if (align == 0 || (align & (align - 1)) != 0) {
-    // if align is not a power of 2, fails
+  if ((align & (align - 1)) != 0) {
+    // if align is not a power of 2 or 0, fails
     return NULL;
+  }
+
+  // set align to be max of two aligns
+  if (alloc->align > align) {
+    align = alloc->align;
   }
 
   // if size does not fit, get more memory from parent and retry
   if (size > alloc->fsize) {
     uint32_t csize = size << 1; // size to allocate for new chunk // TODO overflow checking?
-    // TODO just round up to power of 2?
-    uintptr_t new_mem = (uintptr_t) allocate_internal(alloc->parent, csize, align);
-    if (new_mem) {
-      if (insert_chunk(alloc, new_mem, new_mem + csize)) {
-        // TODO finalize allocation
-      } else {
-        // if new chunk cannot be inserted, fails
-        // TODO free new_mem
-        return NULL;
+    // TODO just round up to power of 2? or round to alloc->align << 2?
+    uintptr_t chunk_mem = (uintptr_t) allocate_internal(alloc->parent, csize, align);
+    if (chunk_mem) {
+      struct chunk_entry* chunk = insert_chunk(alloc, chunk_mem, chunk_mem + csize);
+      if (chunk != NULL) {
+        void* mem = allocate_internal_chunk(chunk, size, align);
+        if (mem != NULL) {
+          return mem;
+        }
+        // if mem cannot be allocated from chunk, remove chunk entry
+        // TODO remove chunk
       }
-
-    } else {
-      // if no new memory for a new chunk can be allocated from parent, fails
-      return NULL;
+      // if new chunk cannot be inserted free chunk memory in parent
+      // TODO free chunk_mem
     }
+    // if no new memory for a new chunk can be allocated from parent, fails
+    return NULL;
   }
 
-  // get number of necessary adjacent cells in bytemap
-  uint32_t fcount = (size + alloc->align - 1) & -alloc->align;
-  // for chunk in chunk list
+  // try allocate from each chunk in chunk list
   for (struct chunk_entry* chunk = alloc->uhead; chunk != NULL; chunk = chunk->next) {
-    uint32_t contig = 0; // counts contiguous memory areas of size allocator->align
-    for (uint32_t i = 0; i < BYTEMAP_SIZE; i++) {
-      // check if mem free
-      if (chunk->fmap[i] == 0) {
-        // increment contiguous counter
-        // TODO handle requested align
-        contig++;
-      } else {
-        // reset contiguous counter
-        contig = 0;
-      }
-      // check if enough contiguous memory found
-      if (contig >= fcount) {
-        // mark as used
-        for (uint32_t j = i - contig + 1; j <= i; j++) {
-          chunk->fmap[j] = 1;
-        }
-        // decrease free mem counters
-        chunk->fsize -= contig * alloc->align;
-        alloc->fsize -= contig * alloc->align;
-        return (void*) (chunk->min + alloc->align * (i - contig + 1));
-      }
+    void* mem = allocate_internal_chunk(chunk, size, align);
+    if (mem != NULL) {
+      return mem;
     }
   }
 
   // could not find free space, so request parent
-  void* new_mem = allocate_internal(alloc->parent, size << 2, align);
-  if (new_mem) {
-    // TODO add to chunk list and finalize allocation
-    return NULL;
-  } else {
-    return NULL;
+  uint32_t csize = size << 1; // size to allocate for new chunk // TODO overflow checking?
+  // TODO just round up to power of 2? or round to alloc->align << 2?
+  uintptr_t chunk_mem = (uintptr_t) allocate_internal(alloc->parent, csize, align);
+  if (chunk_mem) {
+    struct chunk_entry* chunk = insert_chunk(alloc, chunk_mem, chunk_mem + csize);
+    if (chunk != NULL) {
+      void* mem = allocate_internal_chunk(chunk, size, align);
+      if (mem != NULL) {
+        return mem;
+      }
+      // if mem cannot be allocated from chunk, remove chunk entry
+      // TODO remove chunk
+    }
+    // if new chunk cannot be inserted free chunk memory in parent
+    // TODO free chunk_mem
   }
+  // if no new memory for a new chunk can be allocated from parent, fails
+  return NULL;
 }
 
 /*void* allocate(uint32_t size, uint32_t align) {
---
+++ src/c/std.h
@@ -0,0 +1,10 @@
+#ifndef STD_H
+#define STD_H
+
+#include <stddef.h>
+
+void memset(void*, int, size_t);
+void memcpy(void*, const void*, size_t);
+int  memcmp(const void*, const void*, size_t);
+
+#endif // STD_H
